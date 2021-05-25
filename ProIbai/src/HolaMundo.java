49fdf6ae34ec479099e21b49b843a561e3c71aa1
Ibai <Ibai@DESKTOP-KAJ4530>
2021-05-25 05:30:43 +0200

    sa

--- ProIbai/src/HolaMundo.java
+++ ProIbai/src/HolaMundo.java
@@ -3,7 +3,7 @@ public class HolaMundo {
 
 	public static void main(String[] args) {
 System.out.println("Hola mundo");
-System.out.println("segunsdo cambio");
+System.out.println("segunsdo cambioo");
 	}
 
 }
