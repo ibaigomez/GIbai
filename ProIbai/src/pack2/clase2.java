33e7f229d7e7697e74b1482a08b8bfb1d7a0e96f
Ibai <Ibai@DESKTOP-KAJ4530>
2021-05-25 07:22:59 +0200

    rama 1º usuario

---
+++ ProIbai/src/pack2/clase2.java
@@ -0,0 +1,10 @@
+package pack2;
+
+public class clase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("este es el segundo paquete");
+	}
+
+}
